c4d170a9e6efd4df6da6b74c86487211ae8fabd7
Antonio Estela <antonio.otalora@ticketmaster.com>
2024-08-11 17:35:08 -0500

    feat: upgrade node to 20x

--- terraform/email_processing_lambda.tf
+++ terraform/email_processing_lambda.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "email_processing_lambda" {
   function_name    = "email-processing-lambda"
   role             = aws_iam_role.lambda_role.arn
   handler          = "src/index.handler"
-  runtime          = "nodejs18.x"
+  runtime          = "nodejs20.x"
   source_code_hash = data.archive_file.build_zip.output_base64sha256
 }
 
