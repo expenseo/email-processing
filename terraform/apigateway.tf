1a739e831971bb91e96e94dc4ea3eefee57ee6cb
Antonio Estela <antonio.otalora@ticketmaster.com>
2024-08-12 19:04:58 -0500

    feat: add OPTIONS method

--- terraform/apigateway.tf
+++ terraform/apigateway.tf
@@ -9,45 +9,85 @@ resource "aws_api_gateway_resource" "email_processing_resource" {
   path_part   = "process-email"
 }
 
-resource "aws_api_gateway_method" "email_processing_method" {
+# POST METHOD
+resource "aws_api_gateway_method" "post_method" {
   rest_api_id   = aws_api_gateway_rest_api.email_processing_api_rest.id
   resource_id   = aws_api_gateway_resource.email_processing_resource.id
   http_method   = "POST"
   authorization = "NONE"
 }
 
-resource "aws_api_gateway_integration" "email_processing_lambda" {
+resource "aws_api_gateway_integration" "post_method_lambda_integration" {
   rest_api_id             = aws_api_gateway_rest_api.email_processing_api_rest.id
   resource_id             = aws_api_gateway_resource.email_processing_resource.id
-  http_method             = aws_api_gateway_method.email_processing_method.http_method
+  http_method             = aws_api_gateway_method.post_method.http_method
   integration_http_method = "POST"
   type                    = "AWS"
   uri                     = aws_lambda_function.email_processing_lambda.invoke_arn
 }
 
-resource "aws_api_gateway_method_response" "email_process_api_response" {
+resource "aws_api_gateway_method_response" "post_response" {
   rest_api_id = aws_api_gateway_rest_api.email_processing_api_rest.id
   resource_id = aws_api_gateway_resource.email_processing_resource.id
-  http_method = aws_api_gateway_method.email_processing_method.http_method
+  http_method = aws_api_gateway_method.post_method.http_method
   status_code = "200"
 }
 
-resource "aws_api_gateway_integration_response" "email_processing_lambda_response" {
+resource "aws_api_gateway_integration_response" "post_method_lambda_response" {
   rest_api_id = aws_api_gateway_rest_api.email_processing_api_rest.id
   resource_id = aws_api_gateway_resource.email_processing_resource.id
-  http_method = aws_api_gateway_method.email_processing_method.http_method
-  status_code = aws_api_gateway_method_response.email_process_api_response.status_code
+  http_method = aws_api_gateway_method.post_method.http_method
+  status_code = aws_api_gateway_method_response.post_response.status_code
 
   depends_on = [
-    aws_api_gateway_method.email_processing_method,
-    aws_api_gateway_integration.email_processing_lambda
+    aws_api_gateway_method.post_method,
+    aws_api_gateway_integration.post_method_lambda_integration
   ]
 }
 
-# TODO: Create OPTIONS request
+# OPTIONS METHOD
+resource "aws_api_gateway_method" "options_method" {
+  rest_api_id   = aws_api_gateway_rest_api.email_processing_api_rest.id
+  resource_id   = aws_api_gateway_resource.email_processing_resource.id
+  http_method   = "OPTIONS"
+  authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "options_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.email_processing_api_rest.id
+  resource_id             = aws_api_gateway_resource.email_processing_resource.id
+  http_method             = aws_api_gateway_method.options_method.http_method
+  integration_http_method = "OPTIONS"
+  type                    = "MOCK"
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
+
+resource "aws_api_gateway_method_response" "options_method_response" {
+  rest_api_id = aws_api_gateway_rest_api.email_processing_api_rest.id
+  resource_id = aws_api_gateway_resource.email_processing_resource.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = "200"
+}
+
+resource "aws_api_gateway_integration_response" "options_integration_response" {
+  rest_api_id = aws_api_gateway_rest_api.email_processing_api_rest.id
+  resource_id = aws_api_gateway_resource.email_processing_resource.id
+  http_method = aws_api_gateway_method.options_method.http_method
+  status_code = aws_api_gateway_method_response.options_method_response.status_code
+
+  depends_on = [
+    aws_api_gateway_method.options_method
+  ]
+}
+
 
 resource "aws_api_gateway_deployment" "api_deployment" {
-  depends_on = [aws_api_gateway_integration.email_processing_lambda]
+  depends_on = [
+    aws_api_gateway_integration.post_method_lambda_integration,
+    aws_api_gateway_integration.options_integration
+  ]
 
   rest_api_id = aws_api_gateway_rest_api.email_processing_api_rest.id
   stage_name  = "dev"
