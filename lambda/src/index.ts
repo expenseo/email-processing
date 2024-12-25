bc18be2f3f89c09c89c9b510d61cc0a9488de3c1
Antonio Estela <40125100+AntonioEstela@users.noreply.github.com>
2024-12-25 20:35:16 +0000

    feat: add entry lambda log

--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -1,6 +1,8 @@
 import { Context, Handler, Callback } from 'aws-lambda';
 
 export const handler: Handler = async (event, context: Context, callback: Callback) => {
+  console.log('Received event:', JSON.stringify(event, null, 2));
+
   const response = {
     statusCode: 200,
     headers: {
@@ -12,6 +14,5 @@ export const handler: Handler = async (event, context: Context, callback: Callba
       context,
     }),
   };
-
   callback(null, response);
 };
